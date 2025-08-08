68ee2a232e5f7cd6bf8dfa86ed504cfb899aa6da
yqYoi <miyasyrog@gmail.com>
2025-08-09 03:00:29 +0900

    print fqbn

--- PokeControllerForPico/tusb_config.h
+++ PokeControllerForPico/tusb_config.h
@@ -28,7 +28,7 @@
 #define _TUSB_CONFIG_H_
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 //--------------------------------------------------------------------
@@ -36,19 +36,21 @@
 //--------------------------------------------------------------------
 
 #ifndef CFG_TUSB_MCU
- #define CFG_TUSB_MCU             OPT_MCU_RP2040
+#ifdef ARDUINO_FQBN
+#pragma message ARDUINO_FQBN
+#endif
+#define CFG_TUSB_MCU OPT_MCU_RP2040
 #endif
 
-#define CFG_TUSB_RHPORT0_MODE     OPT_MODE_DEVICE
-#define CFG_TUSB_OS               OPT_OS_PICO
+#define CFG_TUSB_RHPORT0_MODE OPT_MODE_DEVICE
+#define CFG_TUSB_OS OPT_OS_PICO
 
 // CFG_TUSB_DEBUG is defined by compiler in DEBUG build
 // #define CFG_TUSB_DEBUG           0
 
-/* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
- * Tinyusb use follows macros to declare transferring memory so that they can be put
- * into those specific section.
- * e.g
+/* USB DMA on some MCUs can only access a specific SRAM region with restriction
+ * on alignment. Tinyusb use follows macros to declare transferring memory so
+ * that they can be put into those specific section. e.g
  * - CFG_TUSB_MEM SECTION : __attribute__ (( section(".usb_ram") ))
  * - CFG_TUSB_MEM_ALIGN   : __attribute__ ((aligned(4)))
  */
@@ -57,7 +59,7 @@
 #endif
 
 #ifndef CFG_TUSB_MEM_ALIGN
-#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
+#define CFG_TUSB_MEM_ALIGN __attribute__((aligned(4)))
 #endif
 
 //--------------------------------------------------------------------
@@ -65,21 +67,21 @@
 //--------------------------------------------------------------------
 
 #ifndef CFG_TUD_ENDPOINT0_SIZE
-#define CFG_TUD_ENDPOINT0_SIZE    64
+#define CFG_TUD_ENDPOINT0_SIZE 64
 #endif
 
 //------------- CLASS -------------//
-#define CFG_TUD_HID               2
-#define CFG_TUD_CDC               0
-#define CFG_TUD_MSC               0
-#define CFG_TUD_MIDI              0
-#define CFG_TUD_VENDOR            0
+#define CFG_TUD_HID 2
+#define CFG_TUD_CDC 0
+#define CFG_TUD_MSC 0
+#define CFG_TUD_MIDI 0
+#define CFG_TUD_VENDOR 0
 
 // HID buffer size Should be sufficient to hold ID (if any) + Data
-#define CFG_TUD_HID_EP_BUFSIZE    (64)
+#define CFG_TUD_HID_EP_BUFSIZE (64)
 
 #ifdef __cplusplus
- }
+}
 #endif
 
 #endif /* _TUSB_CONFIG_H_ */
